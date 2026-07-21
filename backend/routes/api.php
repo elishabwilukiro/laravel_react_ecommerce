f89c2058abecf838368e9702e7ba3320009ac28f
elishabwilukiro <bwilukiroelisha@gmail.com>
2026-07-21 10:17:33 +0300

    Update comment

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -31,11 +31,11 @@ Route::group(['middleware'=>['auth:sanctum','checkUserRole']], function(){
 
 
 Route::group(['middleware'=>['auth:sanctum','checkAdminRole']], function(){
-    // Route::get('categories', [CategoryController::class,'index']);
-    // Route::get('categories/{id}', [CategoryController::class,'show']);
-    // Route::put('categories/{id}', [CategoryController::class,'update']);
-    // Route::delete('categories/{id}', [CategoryController::class,'destroy']);
-    // Route::post('categories', [CategoryController::class,'store']);
+    # Route::get('categories', [CategoryController::class,'index']);
+    # Route::get('categories/{id}', [CategoryController::class,'show']);
+    # Route::put('categories/{id}', [CategoryController::class,'update']);
+    # Route::delete('categories/{id}', [CategoryController::class,'destroy']);
+    # Route::post('categories', [CategoryController::class,'store']);
 
     Route::get('dashboard',[DashboardController::class,'index']);
     Route::get('sizes',[SizeController::class,'index']);
